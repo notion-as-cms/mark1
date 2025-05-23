1c4df2b24d2281d434ae434e7d3ef3bd4c28c5a3
Rajiv Seelam <rajiv@betalectic.com>
2025-05-23 18:26:11 +0530

    feat: add table of contents generation using notion-utils and notion-types packages

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,6 +4,8 @@ import {
   PageObjectResponse,
   type DatabaseObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+import { getPageTableOfContents } from "notion-utils";
+import { type ExtendedRecordMap } from "notion-types";
 
 export async function generateStaticParams() {
   const posts = await getPublishedPosts();
@@ -23,7 +25,7 @@ export async function generateStaticParams() {
 }
 
 const getPage = async (id: string) => {
-  const recordMap = await notionCustom.getPage(id);
+  const recordMap: ExtendedRecordMap = await notionCustom.getPage(id);
 
   return recordMap as typeof recordMap & {
     raw: {
@@ -40,6 +42,10 @@ export default async function Page({
   const { slug } = await params;
   const post = await getPageBySlug(slug);
   const recordMap = await getPage(post.id);
+
+  console.log("recordMap", recordMap.block);
+
+  const tableOfContents = getPageTableOfContents(recordMap, recordMap);
 
   return (
     <div className="max-w-3xl mx-auto p-4 bg-red-50">
